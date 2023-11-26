d8fdeb4681c207949b5f04929736fcde752a100b
Volker Benders <vbenders@gmail.com>
2023-11-26 11:18:54 +0000

    wip

--- src/main/java/io/github/cwacoderwithattitude/crud/Ship.java
+++ src/main/java/io/github/cwacoderwithattitude/crud/Ship.java
@@ -4,10 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Ship {
    @Id
    @GeneratedValue(strategy = GenerationType.AUTO)
@@ -15,9 +19,6 @@ public class Ship {
    private String name;
    private String sign;
    private String type;
-
-   protected Ship() {
-   }
 
    public Ship(String name, String type) {
       this.type = type;
